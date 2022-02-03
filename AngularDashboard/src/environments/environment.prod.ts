7bfedea06346e3ad657eda8a699ce606839cd84e
AnthonyEdwardStark3000 <90055674+AnthonyEdwardStark3000@users.noreply.github.com>
2022-02-04 01:21:44 +0530

    Firebase

---
+++ AngularDashboard/src/environments/environment.prod.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-blog-1bfd0',
+    appId: '1:1019644949424:web:6ab6f75a357c2234a19c75',
+    storageBucket: 'angular-blog-1bfd0.appspot.com',
+    apiKey: 'AIzaSyCbYMS13vNKk6v-FlAOtsWfh-jQpmN0QYc',
+    authDomain: 'angular-blog-1bfd0.firebaseapp.com',
+    messagingSenderId: '1019644949424',
+  },
+  production: true,
+  firebaseConfig:{
+  apiKey: "AIzaSyCbYMS13vNKk6v-FlAOtsWfh-jQpmN0QYc",
+  authDomain: "angular-blog-1bfd0.firebaseapp.com",
+  projectId: "angular-blog-1bfd0",
+  storageBucket: "angular-blog-1bfd0.appspot.com",
+  messagingSenderId: "1019644949424",
+  appId: "1:1019644949424:web:6ab6f75a357c2234a19c75"
+}
+};
---
+++ AngularDashboard/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'angular-blog-1bfd0',
+    appId: '1:1019644949424:web:6ab6f75a357c2234a19c75',
+    storageBucket: 'angular-blog-1bfd0.appspot.com',
+    apiKey: 'AIzaSyCbYMS13vNKk6v-FlAOtsWfh-jQpmN0QYc',
+    authDomain: 'angular-blog-1bfd0.firebaseapp.com',
+    messagingSenderId: '1019644949424',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
