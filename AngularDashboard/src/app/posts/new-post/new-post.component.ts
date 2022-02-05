7430bf1a2222677b07d22c9e142230ab3bb7f6d4
AnthonyEdwardStark3000 <90055674+AnthonyEdwardStark3000@users.noreply.github.com>
2022-02-05 23:16:55 +0530

    Form vaidations

--- AngularDashboard/src/app/posts/new-post/new-post.component.ts
+++ AngularDashboard/src/app/posts/new-post/new-post.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
@@ -19,12 +19,12 @@ export class NewPostComponent implements OnInit {
   constructor(private categoryService: CategoriesService, private fb: FormBuilder)
    {
       this.postForm = this.fb.group({
-        title:[''],
-        permalink:[''],
-        excerpt:[''],
-        category:[''],
-        postImg:[''],
-        content:['']
+        title:['',[Validators.required,Validators.minLength(10)]],
+        permalink:['',Validators.required],
+        excerpt:['',[Validators.required,Validators.minLength(50)]],
+        category:['',Validators.required],
+        postImg:['',Validators.required],
+        content:['',Validators.required]
       })
    }
 
@@ -34,6 +34,12 @@ export class NewPostComponent implements OnInit {
       this.categories = val;
     });
   }
+
+  get fc()
+  {
+    return this.postForm.controls;
+  }
+
   onTitleChanged($event: any)
   {
     // console.log($event.target.value);
@@ -53,5 +59,9 @@ export class NewPostComponent implements OnInit {
     this.selectedImage = $event.target.files[0];
   }
 
+  onSubmit()
+  {
+      
+  }
 
 }
