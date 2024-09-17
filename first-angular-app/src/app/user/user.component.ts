3a60675dfb16e0e1924bf76f16e35ad70f6612e8
MrAnthonyEdwardStark <sureshbabu.innowave@gmail.com>
2024-09-17 08:15:25 +0530

    Created Custom Component - Header

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -8,7 +11,14 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-  check(){
-    alert("check")
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  ImagePath = computed(()=>'assets/users/'+this.selectedUser().avatar);
+  // get ImagePath(){
+  //   return 'assets/users/'+this.selectedUser.avatar;
+  // }
+
+  onSelectUser(){
+    const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
